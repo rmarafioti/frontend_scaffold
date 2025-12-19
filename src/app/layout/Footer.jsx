37b533095b7b7eb2fa02eb3b3fa8c5792bd7a7d4
Rich Marafioti <richmarafioti@gmail.com>
2025-12-19 16:36:39 -0600

    #1 adds layout components, implements all changes in layout.js

---
+++ src/app/layout/Footer.jsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>
+        &copy; {new Date().getFullYear()} Your Company Name. All rights
+        reserved.
+      </p>
+    </footer>
+  );
+}
