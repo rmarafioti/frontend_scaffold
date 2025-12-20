cfb7af9a3a99ff88246364c614fb93fd14031214
Rich Marafioti <richmarafioti@gmail.com>
2025-12-20 11:08:10 -0600

    #5 functional layout across all default features

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,13 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
 
 export default function Home() {
   return (
     <main>
-      <div className={styles.intro}>
-        <h1>Landing Page.</h1>
-        <h2>
-          Lorem ipsum dolor sit amet consectetur adipiscing elit. Dolor sit amet
-          consectetur adipiscing elit quisque faucibus.
-        </h2>
-      </div>
+      <h1>Landing Page</h1>
+      <h2>
+        Lorem ipsum dolor sit amet consectetur adipiscing elit. Dolor sit amet
+        consectetur adipiscing elit quisque faucibus.
+      </h2>
     </main>
   );
 }
